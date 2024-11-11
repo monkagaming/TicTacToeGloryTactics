ba9a03e2d9f87ddfc14ceccfcedc68b622d6b397
monkagaming <103224537+monkagaming@users.noreply.github.com>
2024-11-10 23:27:33 -0500

    scenes/scripts

--- cell.gd
+++ cell.gd
@@ -1,21 +1,18 @@
 extends Button
-
 signal cell_updated(cell)
-var main: Control
-var cell_value: String = ""
+
+var main : Control
+
+var cell_value : String = ""
 
 @onready var background = $Background
 @onready var border = $Border
 
 func _ready():
 	self_modulate.a = 0
-	pressed.connect(_on_pressed)
-
-func _on_pressed():
-	draw_cell()
 
 func draw_x():
-	var tween = create_tween()
+	var tween = get_tree().create_tween()
 	self_modulate = Color("#00ffff")
 	self_modulate.a = 0
 	text = "X"
@@ -23,7 +20,7 @@ func draw_x():
 	tween.tween_property(self, "self_modulate:a", 1, 0.5)
 
 func draw_o():
-	var tween = create_tween()
+	var tween = get_tree().create_tween()
 	self_modulate = Color("#ff4200")
 	self_modulate.a = 0
 	text = "O"
@@ -31,18 +28,22 @@ func draw_o():
 	tween.tween_property(self, "self_modulate:a", 1, 0.5)
 
 func draw_cell():
-	if main == null or main.is_game_end or cell_value != "":
-		return
+	if main.is_game_end: return
 	
-	if main.current_player == "X":
-		draw_x()
-	else:
-		draw_o()
+	if cell_value == "":
+		match main.turn:
+			0:
+				main.turn = 1
+				draw_x()
+			1:
+				main.turn = 0
+				draw_o()
 	
+	mouse_default_cursor_shape = Control.CURSOR_ARROW
 	cell_updated.emit(self)
 
-func glow(color: Color):
-	var tween = create_tween()
+func glow(color):
+	var tween = get_tree().create_tween()
 	background.modulate = color
 	background.modulate.a = 0
 	tween.tween_property(background, "modulate:a", 1, 0.5)
